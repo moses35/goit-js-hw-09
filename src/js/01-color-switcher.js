9c9f6bc7a501ad65bcb21ec46b0c9c752de62e57
moses35 <energetic143@gmail.com>
2023-04-08 01:13:42 +0300

    Update color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+const INTERVAL_DELAY = 1000;
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, INTERVAL_DELAY);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
