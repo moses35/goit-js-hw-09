ee8741681c769f9d0e8b5104535de8014eff8ed5
moses35 <117040713+moses35@users.noreply.github.com>
2023-04-07 22:14:04 +0000

    Deploying to gh-pages from @ moses35/goit-js-hw-09@9c9f6bc7a501ad65bcb21ec46b0c9c752de62e57 🚀

---
+++ 01-color-switcher.a15f5804.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a=null;e.disabled=!0,t.addEventListener("click",(function(){a=setInterval((()=>{d.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.a15f5804.js.map
