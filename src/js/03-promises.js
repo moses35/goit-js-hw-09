6f368226cca1dc389678fda95b703dce8d6c821e
moses35 <energetic143@gmail.com>
2023-04-09 16:33:10 +0300

    Update Promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+//form submit
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { amount, delay, step } = event.currentTarget.elements;
+
+  let promiseAmount = Number(amount.value);
+  let promiseDelay = Number(delay.value);
+  let promiseStep = Number(step.value);
+
+  for (let i = 1; i <= promiseAmount; i += 1) {
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay} ms`
+        );
+      });
+    promiseDelay += promiseStep;
   }
+}
+
+//create Promise
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
